44a069d618566ff5d0555c5815f816c64a6b4a7b
Chaitanya <kalelchaitanya64@gmail.com>
2026-02-21 19:07:29 +0530

    added project info

--- gurukul-vidyamandir/sanity.cli.ts
+++ gurukul-vidyamandir/sanity.cli.ts
@@ -1,4 +1,4 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
@@ -6,10 +6,6 @@ export default defineCliConfig({
     dataset: 'production'
   },
   deployment: {
-    /**
-     * Enable auto-updates for studios.
-     * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
-     */
-    autoUpdates: true,
-  }
+    appId: 'jl0bk7r45jnn1n3edif286va',
+  },
 })
